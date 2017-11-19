71f089d88ee461dece7fdc7d131a2c338b8abea9
Antonio Gil <antonioluisgil@gmail.com>
2017-11-19 18:20:00 +0100

    Added router link to navigate to thread view

--- src/components/ThreadListItem.vue
+++ src/components/ThreadListItem.vue
@@ -2,7 +2,10 @@
   <div class="thread">
     <div>
       <p>
-        <a href="#">{{thread.title}}</a>
+        <a href="#"></a>
+        <router-link :to="{name: 'ThreadShow', params: {id: thread['.key']}}">
+          {{thread.title}}
+        </router-link>
       </p>
       <p class="text-faded text-xsmall">
         By <a href="#">{{user.name}}</a>, {{thread.publishedAt}}.
